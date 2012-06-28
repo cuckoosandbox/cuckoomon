9ff52f04228d9d5bb7d836259e2dbd4bddd0943c
Jurriaan Bremer <jurriaanbremer@gmail.com>
2012-06-28 08:05:59 +0200

    some further work on logging

--- log.c
+++ log.c
@@ -8,7 +8,29 @@ void log(const char *fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
+    int count = 1; char key, nokey;
     while (*fmt) {
+        // we have to find the next format specifier
+        if(--count == 0) {
+            // end of format
+            if(*fmt == 0) break;
+
+            // repeat format specifier
+            if(*fmt >= '2' && *fmt < '9') {
+                count = *fmt++ - '0';
+            }
+
+            // the next format specifier
+            key = *fmt++;
+
+            // omit the key?
+            nokey = 0;
+            if(*fmt == '!') {
+                nokey = 1, fmt++;
+            }
+        }
+
+        // ...
     }
     va_end(args);
 }
--- log.h
+++ log.h
@@ -12,7 +12,19 @@
 // b  -> (int, void *) -> memory with a given size (alias for S)
 // i  -> (int) -> integer
 // l  -> (long) -> long integer
-// p  -> (void *) -> pointer
+// p  -> (void *) -> pointer (alias for l)
+//
+// Each of these format specifiers are prefixed with a key value, unless the
+// exclamation mark specifier is added, e.g.
+//
+// log("s", "key", "value");
+// log("s!", "value");
+//
+// A format specifier can also be repeated for n times (with n in the range
+// 2..9), e.g.
+//
+// log("s!s!s!", "value", "value2", "value3");
+// log("3s!", "value", "value2", "value3");
 //
 
 void log(const char *fmt, ...);
