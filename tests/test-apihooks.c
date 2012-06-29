dee9d8daf41266bc436a2b04c74368b7a22759b4
Jurriaan Bremer <jurriaanbremer@gmail.com>
2012-06-29 12:28:11 +0200

    added apihook test

---
+++ tests/test-apihooks.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+    // there we go
+    LoadLibrary("../cuckoomon.dll");
+
+    fclose(fopen("test-hello", "w"));
+    FILE *fp = fopen("test-hello", "wb");
+    fwrite("whatsup", 1, 6, fp);
+    fclose(fp);
+    fp = fopen("test-hello", "rb");
+    char buf[6];
+    fread(buf, 1, 6, fp);
+    fclose(fp);
+
+    DeleteFile("test-hello");
+
+    HKEY hKey;
+    if(RegCreateKeyEx(HKEY_CURRENT_USER,
+            "Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, NULL, 0,
+            KEY_ALL_ACCESS, NULL, &hKey, NULL) == ERROR_SUCCESS) {
+        RegSetValueEx(hKey, "TestApiHooks", 0, REG_SZ, (BYTE *) "Hoi", 3);
+        Sleep(5000);
+        RegDeleteValue(hKey, "TestApiHooks");
+        RegCloseKey(hKey);
+    }
+}
