c693d7e68a16f61f7c4873261c6beec4dfe9b330
Jurriaan Bremer <jurriaanbremer@gmail.com>
2012-09-06 01:52:06 +0200

    moved structure declarations from misc header to ntapi header

--- misc.h
+++ misc.h
@@ -4,28 +4,3 @@ DWORD GetPidFromProcessHandle(HANDLE process_handle);
 DWORD GetPidFromThreadHandle(HANDLE thread_handle);
 DWORD random();
 int wcsnicmp(const wchar_t *a, const wchar_t *b, int len);
-
-typedef struct _PROCESS_BASIC_INFORMATION {
-    PVOID Reserved1;
-    void *PebBaseAddress;
-    PVOID Reserved2[2];
-    ULONG_PTR UniqueProcessId;
-    PVOID Reserved3;
-} PROCESS_BASIC_INFORMATION;
-
-typedef struct _CLIENT_ID {
-    PVOID UniqueProcess;
-    PVOID UniqueThread;
-} CLIENT_ID, *PCLIENT_ID;
-
-typedef ULONG_PTR KAFFINITY;
-typedef LONG KPRIORITY;
-
-typedef struct _THREAD_BASIC_INFORMATION {
-    NTSTATUS ExitStatus;
-    PVOID TebBaseAddress;
-    CLIENT_ID ClientId;
-    KAFFINITY AffinityMask;
-    KPRIORITY Priority;
-    KPRIORITY BasePriority;
-} THREAD_BASIC_INFORMATION, *PTHREAD_BASIC_INFORMATION;
--- ntapi.h
+++ ntapi.h
@@ -119,4 +119,29 @@ typedef struct _KEY_VALUE_ENTRY {
     ULONG        Type;
 } KEY_VALUE_ENTRY, *PKEY_VALUE_ENTRY;
 
+typedef struct _PROCESS_BASIC_INFORMATION {
+    PVOID Reserved1;
+    void *PebBaseAddress;
+    PVOID Reserved2[2];
+    ULONG_PTR UniqueProcessId;
+    PVOID Reserved3;
+} PROCESS_BASIC_INFORMATION;
+
+typedef struct _CLIENT_ID {
+    PVOID UniqueProcess;
+    PVOID UniqueThread;
+} CLIENT_ID, *PCLIENT_ID;
+
+typedef ULONG_PTR KAFFINITY;
+typedef LONG KPRIORITY;
+
+typedef struct _THREAD_BASIC_INFORMATION {
+    NTSTATUS ExitStatus;
+    PVOID TebBaseAddress;
+    CLIENT_ID ClientId;
+    KAFFINITY AffinityMask;
+    KPRIORITY Priority;
+    KPRIORITY BasePriority;
+} THREAD_BASIC_INFORMATION, *PTHREAD_BASIC_INFORMATION;
+
 #endif
