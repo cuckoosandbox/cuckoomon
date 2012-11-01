b269c810c4586e95bbff5a0ff8b5900f09306169
Jurriaan Bremer <jurriaanbremer@gmail.com>
2012-11-01 09:33:47 +0100

    renamed getpid command to getpids

--- cuckoomon.c
+++ cuckoomon.c
@@ -257,7 +257,7 @@ BOOL APIENTRY DllMain(HANDLE hModule, DWORD dwReason, LPVOID lpReserved)
 
         // obtain all protected pids
         int pids[MAX_PROTECTED_PIDS], length = sizeof(pids);
-        pipe2(pids, &length, "GETPID");
+        pipe2(pids, &length, "GETPIDS");
         for (int i = 0; i < length / sizeof(pids[0]); i++) {
             add_protected_pid(pids[i]);
         }
