2419bb10d1c1733b8f1b396857f586affe825b4f
Jurriaan Bremer <jurriaanbremer@gmail.com>
2013-01-31 12:02:21 +0100

    added various file path handling functions

--- misc.c
+++ misc.c
@@ -18,6 +18,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <stdio.h>
 #include <windows.h>
+#include <ctype.h>
+#include <shlwapi.h>
 #include "ntapi.h"
 #include "misc.h"
 
@@ -140,5 +142,115 @@ void hide_module_from_peb(HMODULE module_handle)
             memset(mod, 0, sizeof(LDR_MODULE));
             break;
         }
+    }
+}
+
+int path_from_handle(HANDLE handle, wchar_t *path)
+{
+    static NTSTATUS (WINAPI *pNtQueryVolumeInformationFile)(
+        _In_   HANDLE FileHandle,
+        _Out_  PIO_STATUS_BLOCK IoStatusBlock,
+        _Out_  PVOID FsInformation,
+        _In_   ULONG Length,
+        _In_   FS_INFORMATION_CLASS FsInformationClass
+    );
+
+    if(pNtQueryVolumeInformationFile == NULL) {
+        *(FARPROC *) &pNtQueryVolumeInformationFile = GetProcAddress(
+            GetModuleHandle("ntdll"), "NtQueryVolumeInformationFile");
+    }
+
+    static NTSTATUS (WINAPI *pNtQueryInformationFile)(
+        _In_   HANDLE FileHandle,
+        _Out_  PIO_STATUS_BLOCK IoStatusBlock,
+        _Out_  PVOID FileInformation,
+        _In_   ULONG Length,
+        _In_   FILE_INFORMATION_CLASS FileInformationClass
+    );
+
+    if(pNtQueryInformationFile == NULL) {
+        *(FARPROC *) &pNtQueryInformationFile = GetProcAddress(
+            GetModuleHandle("ntdll"), "NtQueryInformationFile");
+    }
+
+    IO_STATUS_BLOCK status = {};
+    FILE_FS_VOLUME_INFORMATION volume_information;
+
+    unsigned char buf[FILE_NAME_INFORMATION_REQUIRED_SIZE];
+    FILE_NAME_INFORMATION *name_information = (FILE_NAME_INFORMATION *) buf;
+
+    // get the volume serial number of the directory handle
+    if(NT_SUCCESS(pNtQueryVolumeInformationFile(handle, &status,
+            &volume_information, sizeof(volume_information),
+            FileFsVolumeInformation)) == 0) {
+        return 0;
+    }
+
+    unsigned long serial_number;
+
+    // enumerate all harddisks in order to find the
+    // corresponding serial number
+    wcscpy(path, L"?:\\");
+    for (path[0] = 'A'; path[0] <= 'Z'; path[0]++) {
+        if(GetVolumeInformationW(path, NULL, 0, &serial_number, NULL,
+                NULL, NULL, 0) == 0 ||
+                serial_number != volume_information.VolumeSerialNumber) {
+            continue;
+        }
+
+        // obtain the relative path for this filename on the given harddisk
+        if(NT_SUCCESS(pNtQueryInformationFile(handle, &status,
+                name_information, FILE_NAME_INFORMATION_REQUIRED_SIZE,
+                FileNameInformation))) {
+
+            int length = name_information->FileNameLength / sizeof(wchar_t);
+
+            // NtQueryInformationFile omits the "C:" part in a
+            // filename, apparently
+            wcsncpy(path + 2, name_information->FileName, length);
+            path[2 + length] = 0;
+            return 2 + length;
+        }
+    }
+    return 0;
+}
+
+int path_from_object_attributes(const OBJECT_ATTRIBUTES *obj, wchar_t *path)
+{
+    if(obj->RootDirectory == NULL) {
+        wcsncpy(path, obj->ObjectName->Buffer, obj->ObjectName->Length);
+        path[obj->ObjectName->Length / sizeof(wchar_t)] = 0;
+        return obj->ObjectName->Length / sizeof(wchar_t);
+    }
+
+    int len = path_from_handle(obj->RootDirectory, path);
+    path[len++] = L'\\';
+    wcsncpy(&path[len], obj->ObjectName->Buffer,
+        obj->ObjectName->Length / sizeof(wchar_t));
+    path[len + obj->ObjectName->Length / sizeof(wchar_t)] = 0;
+    return len + obj->ObjectName->Length / sizeof(wchar_t);
+}
+
+int ensure_absolute_path(wchar_t *out, const wchar_t *in, int length)
+{
+    if(!wcsncmp(in, L"\\??\\", 4)) {
+        length -= 4, in += 4;
+        wcsncpy(out, in, length < MAX_PATH ? length : MAX_PATH);
+        return out[length] = 0, length;
+    }
+    else if(in[1] != ':' || (in[2] != '\\' && in[2] != '/')) {
+        wchar_t cur_dir[MAX_PATH], fname[MAX_PATH];
+        GetCurrentDirectoryW(ARRAYSIZE(cur_dir), cur_dir);
+
+        // ensure the filename is zero-terminated
+        wcsncpy(fname, in, length < MAX_PATH ? length : MAX_PATH);
+        fname[length] = 0;
+
+        PathCombineW(out, cur_dir, fname);
+        return lstrlenW(out);
+    }
+    else {
+        wcsncpy(out, in, length < MAX_PATH ? length : MAX_PATH);
+        return out[length] = 0, length;
     }
 }
--- misc.h
+++ misc.h
@@ -24,6 +24,10 @@ DWORD randint(DWORD min, DWORD max);
 BOOL is_directory_objattr(const OBJECT_ATTRIBUTES *obj);
 void hide_module_from_peb(HMODULE module_handle);
 
+int path_from_handle(HANDLE handle, wchar_t *path);
+int path_from_object_attributes(const OBJECT_ATTRIBUTES *obj, wchar_t *path);
+int ensure_absolute_path(wchar_t *out, const wchar_t *in, int length);
+
 // imported but for some doesn't show up when #including string.h etc
 int wcsnicmp(const wchar_t *a, const wchar_t *b, int len);
 int wcsicmp(const wchar_t *a, const wchar_t *b);
