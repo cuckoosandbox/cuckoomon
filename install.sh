d782377d618d2eb48673ff99010c8dede17084f4
Jurriaan Bremer <jurriaanbremer@gmail.com>
2013-11-04 09:30:15 +0100

    allow setting of cuckoodir/cuckoomon through env in install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 # directory in which the main repository of Cuckoo Sandbox can be found
-CUCKOODIR=../cuckoo
+if [ -z "$CUCKOODIR" ]; then
+    CUCKOODIR=../cuckoo
+fi
 
 # target filename - in case of testing with custom dll's one may wish to
 # use a different filename, such as "cuckoomon_bson.dll"
-CUCKOOMON=cuckoomon.dll
+if [ -z "$CUCKOOMON" ]; then
+    CUCKOOMON=cuckoomon.dll
+fi
 
 make && cp cuckoomon.dll "$CUCKOODIR/analyzer/windows/dll/$CUCKOOMON"
