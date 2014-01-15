7634e5ccae311d955ba80cdf90b4a6e7851ed39e
Jurriaan Bremer <jurriaanbremer@gmail.com>
2014-01-15 15:08:40 +0100

    bumped the year

--- hook_socket.c
+++ hook_socket.c
@@ -1,6 +1,6 @@
 /*
 Cuckoo Sandbox - Automated Malware Analysis
-Copyright (C) 2010-2013 Cuckoo Sandbox Developers
+Copyright (C) 2010-2014 Cuckoo Sandbox Developers
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
