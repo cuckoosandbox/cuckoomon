fc02728b407696a23942f0f234a2a34b98c36ccc
Jurriaan Bremer <jurriaanbremer@gmail.com>
2014-02-18 13:55:30 +0100

    poc for testing unhook detection

---
+++ tests/unhook.c
@@ -0,0 +1,34 @@
+/*
+Cuckoo Sandbox - Automated Malware Analysis
+Copyright (C) 2010-2014 Cuckoo Sandbox Developers
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <windows.h>
+
+int main(int argc, char *argv[])
+{
+    FARPROC fp = GetProcAddress(
+        GetModuleHandle("kernel32"), "IsDebuggerPresent");
+
+    unsigned long old_protect;
+    VirtualProtect(fp, 0x1000, PAGE_EXECUTE_READWRITE, &old_protect);
+
+    // Let's see.
+    memset(fp, 0xcc, 32);
+
+    Sleep(10000);
+}
