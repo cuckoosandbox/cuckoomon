353ca5fdb66b95c795d888649aa9059e6b52b8c3
Jurriaan Bremer <jurriaanbremer@gmail.com>
2014-07-20 16:34:38 +0200

    fix capstone argument bug

--- hooking.c
+++ hooking.c
@@ -46,7 +46,7 @@ int lde(void *addr)
     static int capstone_init = 0; static csh capstone;
 
     if(capstone_init == 0) {
-        cs_open(CS_ARCH_X86, CS_MODE_LITTLE_ENDIAN, &capstone);
+        cs_open(CS_ARCH_X86, CS_MODE_32, &capstone);
         capstone_init = 1;
     }
 
